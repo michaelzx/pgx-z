bf548d9d753ae772577034a49b2aa35813735841
StevenZhang <mail@zhangxiao.org>
2023-08-19 14:38:29 +0800

    增加cout方法

---
+++ pgxz/query_count.go
@@ -0,0 +1,36 @@
+package pgxz
+
+import (
+	"context"
+	"strconv"
+	"strings"
+)
+
+func Count[T IModel](db *PgDb, whereSql string, whereArgs ...any) (int64, error) {
+	var t T
+	var sql strings.Builder
+	sqlArgs := make([]any, 0)
+	var sqlArgIdx int64
+	var sqlArgAppend = func(v any) {
+		sqlArgs = append(sqlArgs, v)
+		sqlArgIdx++
+	}
+	// select
+	sql.WriteString("select count(*) from ")
+	sql.WriteString("\"")
+	sql.WriteString(t.TableName())
+	sql.WriteString("\"")
+	if whereSql != "" {
+		// where
+		sql.WriteString(" where ")
+		for _, whereArg := range whereArgs {
+			sqlArgAppend(whereArg)
+			whereSql = strings.Replace(whereSql, "?", "$"+strconv.FormatInt(sqlArgIdx, 10), 1)
+		}
+		sql.WriteString(whereSql)
+	}
+	sql.WriteString(";")
+	var c int64
+	err := db.QueryRow(context.TODO(), sql.String(), sqlArgs...).Scan(&c)
+	return c, err
+}
